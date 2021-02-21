15284f6444abfd299157da2cbb17e7e2f48f7890
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-02-21 16:15:22 +0100

    Removed chaff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,19 @@
-import Head from "next/head";
-import styled from "styled-components";
+import Head from 'next/head';
+import styled from 'styled-components';
 
 export default function Homepage() {
-  return (
-    <>
-      <Head>
-        <title>Home - Ochuko Ekrresa | Software Engineer</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Main>Hello Ochuko 👋</Main>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Home - Ochuko Ekrresa | Software Engineer</title>
+			</Head>
+			<Main>Hello Ochuko 👋</Main>
+		</>
+	);
 }
 
 const Main = styled.main`
-  min-height: 100vh;
-  display: grid;
-  place-items: center;
+	min-height: 100vh;
+	display: grid;
+	place-items: center;
 `;
