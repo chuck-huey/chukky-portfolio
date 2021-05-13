99f0b43ac7ad00520bec8d1d5eb62b12ffc4ae36
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-05-13 18:57:19 +0100

    Animate navbar

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import * as React from 'react';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
+
 import { Hamburger } from '../Hamburger';
 
 type NavProps = {
@@ -9,6 +11,27 @@ type NavProps = {
 		url: string;
 		text: string;
 	}[];
+};
+
+const listVariant = {
+	hidden: {
+		opacity: 0,
+		transition: {
+			when: 'afterChildren',
+		},
+	},
+	visible: {
+		opacity: 1,
+		transition: {
+			when: 'beforeChildren',
+			staggerChildren: 0.1,
+		},
+	},
+};
+
+const itemVariant = {
+	hidden: { opacity: 0, y: -30 },
+	visible: { opacity: 1, y: 0, transition: { type: 'tween' } },
 };
 
 export function NavBar(props: NavProps) {
@@ -32,13 +55,18 @@ export function NavBar(props: NavProps) {
 			<Hamburger navStatus={nav} handleNav={() => toggleNav(!nav)} />
 			{/* Mobile menu */}
 
-			<nav className="nav">
+			<motion.nav
+				initial="hidden"
+				animate="visible"
+				variants={listVariant}
+				className="nav"
+			>
 				{navList.map(item => (
-					<div key={item.url} className="nav__item">
+					<motion.div variants={itemVariant} key={item.url} className="nav__item">
 						<Link href={item.url}>{item.text}</Link>
-					</div>
+					</motion.div>
 				))}
-			</nav>
+			</motion.nav>
 		</StyledHeader>
 	);
 }
@@ -51,16 +79,17 @@ const StyledHeader = styled.header`
 	padding-bottom: 1.5em;
 
 	.logo {
-		font-size: 1.9rem;
-		color: #0e7dcc;
-		font-weight: 800;
+		text-transform: lowercase;
+		color: #0e5e97;
+		letter-spacing: 1px;
+		font-size: 2.5rem;
 		letter-spacing: 0.5px;
 		text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
 		a {
-			font-family: var(--font-fam-heading);
+			font-weight: 700;
+			font-family: var(--font-fam-text);
 			color: inherit;
-			-webkit-text-stroke-color: #285561;
 			-webkit-text-stroke-width: 1.5px;
 
 			&:hover {
@@ -117,15 +146,15 @@ const StyledHeader = styled.header`
 			padding: 1.5px;
 
 			&:nth-of-type(1) {
-				width: 80%;
+				width: 100%;
 			}
 
 			&:nth-of-type(2) {
-				width: 100%;
+				width: 80%;
 			}
 
 			&:nth-of-type(3) {
-				width: 100%;
+				width: 65%;
 			}
 		}
 
